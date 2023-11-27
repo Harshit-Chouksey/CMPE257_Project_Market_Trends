959323235613a672b77289528dc85db4146f4e4e
DarkLegacy22 <someshbgd3@gmail.com>
2023-11-26 16:15:30 -0800

    LSTM Implemented

---
+++ LSTM/main.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+import matplotlib.pyplot as plt
+import math
+from sklearn.metrics import mean_squared_error
+
+df = pd.read_csv('./LSTM/stock_price.csv')
+data = df.reset_index()['Close']
+
+# plt.plot(data)
+# plt.show()
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+data = scaler.fit_transform(np.array(data).reshape(-1, 1))
+
+training_size = int(len(data) * 0.70)
+test_size = len(data) - training_size
+train_data, test_data = data[0:training_size, :], data[training_size:len(data), :1]
+
+def create_dataset(dataset, time_step=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset) - time_step - 1):
+        a = dataset[i:(i + time_step), 0]
+        dataX.append(a)
+        dataY.append(dataset[i + time_step, 0])
+    return np.array(dataX), np.array(dataY)
+
+
+time_step = 100
+X_train, y_train = create_dataset(train_data, time_step)
+X_test, y_test = create_dataset(test_data, time_step)
+
+print(X_train.shape), print(y_train.shape)
+print(X_test.shape), print(y_test.shape)
+
+X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+
+model = Sequential()
+model.add(LSTM(50, return_sequences=True, input_shape=(100, 1)))
+model.add(LSTM(50, return_sequences=True))
+model.add(LSTM(50))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+model.summary()
+model.summary()
+
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100, batch_size=64, verbose=1)
+
+train_predict = model.predict(X_train)
+test_predict = model.predict(X_test)
+
+train_predict = scaler.inverse_transform(train_predict)
+test_predict = scaler.inverse_transform(test_predict)
+
+a = math.sqrt(mean_squared_error(y_train, train_predict))
+print(a)
+
+b = math.sqrt(mean_squared_error(y_test, test_predict))
+print(b)
+
+
+print(np.empty_like(data))
+
+look_back = 100
+trainPredictPlot = np.empty_like(data)
+print(trainPredictPlot)
+print(data)
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[look_back:len(train_predict) + look_back, :] = train_predict
+# shift test predictions for plotting
+testPredictPlot = np.empty_like(data)
+testPredictPlot[:, :] = np.nan
+testPredictPlot[len(train_predict) + (look_back * 2) + 1:len(data) - 1, :] = test_predict
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(data))
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
