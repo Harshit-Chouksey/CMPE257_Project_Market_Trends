7e1deded0e47eecb0a3cc1698702da88b24894ba
DarkLegacy22 <someshbgd3@gmail.com>
2023-11-28 01:15:07 -0800

    Stacked LSTM Copmplete with graphs

--- LSTM/main.py
+++ LSTM/main.py
@@ -6,9 +6,12 @@ from keras.layers import LSTM, Dense
 import matplotlib.pyplot as plt
 import math
 from sklearn.metrics import mean_squared_error
+import time
+
+start_time = time.time()
 
 df = pd.read_csv('stock_price.csv')
-data = df.reset_index()['Close']
+data = df['Close']
 
 scaler = MinMaxScaler(feature_range=(0, 1))
 data = scaler.fit_transform(np.array(data).reshape(-1, 1))
@@ -17,14 +20,16 @@ training_size = int(len(data) * 0.70)
 test_size = len(data) - training_size
 train_data, test_data = data[0:training_size, :], data[training_size:len(data), :1]
 
+print(len(train_data), len(test_data))
+
+
 def create_dataset(dataset, time_step=1):
     dataX, dataY = [], []
     for i in range(len(dataset) - time_step - 1):
-        a = dataset[i:(i + time_step), 0]
-        dataX.append(a)
+        temp = dataset[i:(i + time_step), 0]
+        dataX.append(temp)
         dataY.append(dataset[i + time_step, 0])
     return np.array(dataX), np.array(dataY)
-
 
 time_step = 100
 X_train, y_train = create_dataset(train_data, time_step)
@@ -41,40 +46,49 @@ model.add(LSTM(50, return_sequences=True, input_shape=(100, 1)))
 model.add(LSTM(50, return_sequences=True))
 model.add(LSTM(50))
 model.add(Dense(1))
-model.compile(loss='mean_squared_error', optimizer='adam')
-
+model.compile(loss='mean_squared_error', optimizer='adam')  # Stochastic Gradient Descent
 model.summary()
-model.summary()
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100, verbose=1)
 
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100, batch_size=64, verbose=1)
+training_time = time.time() - start_time
+print("Training Time:", training_time)
 
+start_time = time.time()
 train_predict = model.predict(X_train)
 test_predict = model.predict(X_test)
-
 train_predict = scaler.inverse_transform(train_predict)
 test_predict = scaler.inverse_transform(test_predict)
 
-a = math.sqrt(mean_squared_error(y_train, train_predict))
-print(a)
+predicting_time = time.time() - start_time
+print("Predicting Time:", predicting_time)
 
-b = math.sqrt(mean_squared_error(y_test, test_predict))
-print(b)
-
-
-print(np.empty_like(data))
+loss = model.evaluate(X_test, y_test)
+print("Test Loss:", loss)
 
 look_back = 100
 trainPredictPlot = np.empty_like(data)
-print(trainPredictPlot)
-print(data)
 trainPredictPlot[:, :] = np.nan
 trainPredictPlot[look_back:len(train_predict) + look_back, :] = train_predict
-# shift test predictions for plotting
+
 testPredictPlot = np.empty_like(data)
 testPredictPlot[:, :] = np.nan
 testPredictPlot[len(train_predict) + (look_back * 2) + 1:len(data) - 1, :] = test_predict
+
 # plot baseline and predictions
-plt.plot(scaler.inverse_transform(data))
-plt.plot(trainPredictPlot)
-plt.plot(testPredictPlot)
+plt.ylabel('Stock Price ($)')
+plt.xlabel('Days')
+plt.title('Stock Prices Over Last 5 Years')
+
+plt.plot(scaler.inverse_transform(data), label="Actual Stock Price")
+plt.plot(trainPredictPlot, label="Training ")
+plt.plot(testPredictPlot, label="Predicted")
+plt.legend()
+plt.show()
+
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.title('Training and Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
 plt.show()
